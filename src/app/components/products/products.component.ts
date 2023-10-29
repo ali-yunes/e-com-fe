71f7f10109870ba03ffb4aa5cf0b7dc12f7d0d00
Ali Yunes <younesal99@gmail.com>
2023-10-29 20:55:45 +0200

    Update home page to display multiple product sections

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 
 interface ProductI {
@@ -19,12 +19,17 @@ interface ProductI {
 
 
 export class ProductsComponent implements OnInit {
+  @Input() searchTerm: string;
+  @Input() category: string;
+  page: number = 0;
+  limit: number = 10;
+  
   products: ProductI[];
   total: number;
 
   constructor(private productService: ProductService){}
 
   ngOnInit(): void {
-    this.productService.getPaginatedProducts('','','','').subscribe((data)=>{console.log(data.products); this.products = data.products});
+    this.productService.getPaginatedProducts(this.searchTerm,this.category,this.page.toString(),this.limit.toString()).subscribe((data)=>{console.log(data.products); this.products = data.products});
   }
 }
