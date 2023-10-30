bf06f8dd549688324895c7bac3f8667769a42ca9
Ali Yunes <younesal99@gmail.com>
2023-10-30 15:52:10 +0200

    Add horizontal scrolling buttons to products and categories on home page

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements AfterViewInit{
+  @ViewChild('scrollableCategories') scrollableDiv: ElementRef;
+  div: HTMLDivElement;
+
+  ngAfterViewInit(): void {
+    this.div = this.scrollableDiv.nativeElement;
+  }
+
+  scrollLeft(): void {
+    this.div.scrollBy({ left: -200, behavior: 'smooth' });
+
+  }
+
+  scrollRight(): void {
+    this.div.scrollBy({ left: 200, behavior: 'smooth' });
+  }
 
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import {ProductI} from "../../models/products";
 
@@ -8,17 +8,30 @@ import {ProductI} from "../../models/products";
 })
 
 
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, AfterViewInit {
   @Input() searchTerm: string;
   @Input() category: string;
+  @ViewChild('scrollableProducts') scrollableDiv: ElementRef;
+  div: HTMLDivElement;
   page: number = 0;
   limit: number = 10;
   products: ProductI[];
-  total: number;
 
   constructor(private productService: ProductService){}
 
   ngOnInit(): void {
     this.productService.getPaginatedProducts(this.searchTerm,this.category,this.page.toString(),this.limit.toString()).subscribe((data)=>{this.products = data.products});
+  }
+
+  ngAfterViewInit(): void {
+    this.div = this.scrollableDiv.nativeElement;
+  }
+
+  scrollLeft(): void {
+      this.div.scrollBy({ left: -200, behavior: 'smooth' });
+  }
+
+  scrollRight(): void {
+      this.div.scrollBy({ left: 200, behavior: 'smooth' });
   }
 }
